975bcedf7588aad65d0a98e245272ee2215b7ad4
clamp <112768300+abhishektorgal@users.noreply.github.com>
2024-02-16 12:28:37 +0530

    Update dbcon.php

--- Includes/dbcon.php
+++ Includes/dbcon.php
@@ -1,8 +1,8 @@
 <?php
-	$host = "localhost";
-	$user = "root";
-	$pass = "";
-	$db = "attendancemsystem";
+	$host = "sql210.infinityfree.com";
+	$user = "if0_35990446";
+	$pass = "4mxNFpqjAd3RIMT";
+	$db = "if0_35990446_attendancemsystem";
 	
 	$conn = new mysqli($host, $user, $pass, $db);
 	if($conn->connect_error){
